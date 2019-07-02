2432660cc9d48243cb6e78aaf2a94ec7ae116226
Didaktik <didaktikm@github.com>
2019-07-02 15:54:59 +0300

    add localtets

--- pam.sh
+++ pam.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
-# Проверка даты на принадлежность к нерабочему дню, согласно официальным указам и распоряжениям.
-# Подробнее: https://isdayoff.ru/desc/
-TODAY=$(curl https://isdayoff.ru/$(date +%Y%m%d)?ru=CC)
 # Проверяем принадлежность пользователя к группе admin
 GROUP=$(groups $PAM_USER | grep -c admin)
-
-if [[ $TODAY -ne 1 ]]; then  # Сравниваем значекние переменной. Если не выходной или праздник,возвращяем 0
+# Проверка даты на принадлежность к нерабочему дню, согласно официальным указам и распоряжениям.
+# Подробнее: https://isdayoff.ru/desc/
+TODAY=$(curl https://isdayoff.ru/"$(date +%Y%m%d)"?ru=CC)
+if [[ "$?" -eq 0 ]]; then
+  if [[ $TODAY -ne 1 ]]; then  # Сравниваем значение переменной. Если не выходной или праздник,возвращяем 0
       exit 0
-else
-  if [[ $GROUP -eq 1 ]]; then # Проверяем группу. Если группа admin возвращаем 0 если нет то возвращаем ошибку. 
-      exit 0
-    else
-      exit 1
+  else
+    if [[ $GROUP -eq 1 ]]; then # Проверяем группу. Если группа admin возвращаем 0 если нет то возвращаем ошибку. 
+       exit 0
+      else
+        exit 1
+    fi
   fi
+else
+    if [[ $(date +%u) -gt 5 ]]; then
+        exit 1
+    elif [[ $(grep -q `date +%d.%m` /etc/holidays) ]]; then
+        if [[ $GROUP -eq 1 ]]; then
+            exit 1
+        else
+            exit 0
+        fi    
+    fi
 fi
 
